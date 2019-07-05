69f4ed0148a81c7056cf41dd4327e52a4cb8b895
gabrielgsv <gabriel.soares.vilela@gmail.com>
2019-07-05 17:33:21 -0300

    feat: add initials screens(Heroes, Comics and Creators), organized with react router and material menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import { BrowserRouter, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './app.css'
+
+import Router from './Router'
+
+const useStyles = makeStyles({
+	root: {
+		flexGrow: 1,
+	},
+})
+
+export default function App() {
+	const classes = useStyles();
+
+	return (
+		<BrowserRouter>
+			<Paper className={classes.root}>
+				<Tabs
+					indicatorColor="primary"
+					textColor="primary"
+					centered
+				>
+					<Tab label="Heroes" component={Link} to="/" />
+					<Tab label="Comics" component={Link} to="/comics" />
+					<Tab label="Creator" component={Link} to="/creators" />
+				</Tabs>
+			</Paper>
+			<Router />
+		</BrowserRouter>
+	);
 }
-
-export default App;
